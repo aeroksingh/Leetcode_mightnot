80d1c50257e9610a637621860c6c9e912b2deddd
Ashutosh Singh <assign131@gmail.com>
2025-08-28 23:23:40 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 5.58%] - LeetPush

---
+++ Climbing-Stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if ( n<=1) return 1;
+        int first=1,second=2;
+        for(int i=2;i<n;i++){
+            int temp = first+second;
+            first=second;
+            second= temp;
+        }
+        return second;
+    }
+}
