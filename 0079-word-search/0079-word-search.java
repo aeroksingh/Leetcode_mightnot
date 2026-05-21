75cf99ddb2be4687ade4cebc14285fbc08fb95aa
Ashutosh Singh <assign131@gmail.com>
2026-05-21 21:47:04 +0530

    Time: 145 ms (44.64%), Space: 42.8 MB (87.24%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(dfs(board,word,i,j,0,visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+    private boolean dfs(char[][] board,String word,int i ,int j,int index,boolean[][] visited){
+        // up ke liye 0<=i<m
+        // down ke liye j<n
+        // side ke liye k < i-1
+        // left ke liye l > 0
+
+        if(index == word.length()) return true;
+
+        if(i < 0 || j < 0 ||
+           i >= board.length || j >= board[0].length ||
+           visited[i][j] ||
+           board[i][j] != word.charAt(index)) {
+
+            return false;
+        }
+
+        visited[i][j] = true;
+
+        boolean valid = dfs(board,word,i+1,j,index+1,visited) ||
+                        dfs(board,word,i-1,j,index+1,visited) ||
+                        dfs(board,word,i,j+1,index+1,visited) ||
+                        dfs(board,word,i,j-1,index+1,visited)  ;
+
+        
+    
+        visited[i][j] = false;
+
+        return valid;
+
+    }
+}
