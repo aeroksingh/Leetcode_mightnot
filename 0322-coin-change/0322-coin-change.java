cb3763ed72cbc7a0a5761ac29b68ab6c956d83e1
Ashutosh Singh <assign131@gmail.com>
2026-09-15 17:34:01 +0530

    Time: 36 ms (11.45%), Space: 48.1 MB (5.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,22 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        
         int n = coins.length;
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0] = 0;
-
-        for(int i=0;i<=amount;i++)
-        {
-            for(int coin : coins)
-            {
-                if(i>=coin)
-                {
-                    dp[i] = Math.min(dp[i],dp[i-coin]+1);
-                }
+        long[][] dp = new long[n][amount+1];
+        for(long[] d : dp){
+            Arrays.fill(d,-1);
+        }
+        int ans = (int) help(0,coins,amount,dp);
+        if(ans == Integer.MAX_VALUE) return -1;
+        return ans;
+    }
+    private long help(int index, int[] coins, int amount, long[][] dp){
+        if(index == coins.length){
+            if(amount == 0){
+                return 0;
+            }else{
+                return Integer.MAX_VALUE;
             }
         }
-        return dp[amount]>amount ? -1 : dp[amount];
-
+        if(dp[index][amount] != -1) return dp[index][amount];
+        long skip = help(index+1,coins,amount,dp);
+        if(amount<coins[index]){
+            return skip;
+        }
+        long pick = 1 + help(index,coins,amount-coins[index],dp);
+        return dp[index][amount] = Math.min(skip,pick);
     }
 }
